9388c0a7fe0bb1c7718663f7dd411acda09c2642
Azee <aziztitu1996@gmail.com>
2018-09-14 00:33:24 -0500

    Adds Sidekick. Adds Patrol State. Adds DirFlipper.

---
+++ Assets/Scripts/AI/SideKicks/SideKick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AI.SideKickStates;
+using UnityEngine;
+
+public class SideKick : MonoBehaviour
+{
+    public bool InitiallyFacingRight = true;
+
+    private StateMachine<SideKick> _stateMachine;
+    [HideInInspector] public SideKickController Controller;
+
+    public Patrol.StateData PatrolStateData;
+
+    void Awake()
+    {
+        Controller = GetComponent<SideKickController>();
+
+        _stateMachine = new StateMachine<SideKick>(this);
+        _stateMachine.SwitchState(Patrol.Instance, InitiallyFacingRight ? Vector3.right : Vector3.left);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _stateMachine.Update();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.CompareTag("DirFlipper"))
+        {
+            if (PatrolStateData != null)
+            {
+                PatrolStateData.CurDir.x *= -1;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Attacking Player");
+        }
+    }
+}
---
+++ Assets/Scripts/AI/SideKicks/States/Patrol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AI.SideKickStates
+{
+    public class Patrol : StateMachine<SideKick>.State
+    {
+        public class StateData
+        {
+            public Vector3 CurDir;
+        }
+
+        private static Patrol _instance;
+
+        public static Patrol Instance
+        {
+            get { return _instance ?? (_instance = new Patrol()); }
+        }
+
+        private Patrol()
+        {
+        }
+
+        public void Enter(SideKick owner, params object[] args)
+        {
+            Vector3 startDir = (Vector3) args[0];
+            owner.PatrolStateData = new StateData {CurDir = startDir};
+        }
+
+        public void Update(SideKick owner)
+        {
+            owner.Controller.Move(owner.PatrolStateData.CurDir);
+        }
+
+        public void Exit(SideKick owner)
+        {
+
+        }
+    }
+}
