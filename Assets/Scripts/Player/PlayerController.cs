5c9f6233896beca7facb60233182924932523a63
William Lacalle <digitalbros7@gmail.com>
2018-09-14 01:47:58 -0500

    Adds Player Controller, with temp sprite work.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject Floor;
+    public int walk;
+    public int run;
+    public int jump;
+    private bool grounded;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sp;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+
+    void FixedUpdate () {
+        grounded = rb2d.IsTouching(Floor.GetComponent<BoxCollider2D>());
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movementW = new Vector2(moveHorizontal * walk , 0);
+        Vector2 movementR = new Vector2(moveHorizontal * run, 0);
+        
+        if (moveHorizontal > 0.1f) {
+            sp.flipX = false;
+        }
+        if (moveHorizontal < -0.1f)
+        {
+            sp.flipX = true;
+        }
+
+        if (!(Input.GetButton("Run")))
+        {
+            transform.Translate(movementW * Time.fixedDeltaTime);
+        }
+
+        if (moveHorizontal != 0 && Input.GetButton("Run")) {
+                transform.Translate((movementR) * Time.fixedDeltaTime);
+        }
+
+        if (grounded && Input.GetButtonDown("Jump")) {
+            grounded = false;
+            rb2d.AddForce(Vector2.up * jump);
+        }
+
+    }
+}
