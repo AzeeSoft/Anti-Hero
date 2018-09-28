124ef4106208e49526afde9f9939b5e22dc0b126
Andrew Ward <andrewward22499@gmail.com>
2018-09-28 16:51:58 -0500

    Adds Menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	public string  NextScene;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(NextScene);
+	}
+	
+	public void QuitGame()
+	{
+		Debug.Log("You tried to Quit");
+		Application.Quit();
+	}
+}
