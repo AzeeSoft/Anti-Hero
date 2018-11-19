73b6dcf4f2814f90226c09784538f05ece0e4d94
Azee <aziztitu1996@gmail.com>
2018-11-18 20:14:57 -0600

    Adds and Implements Health and Dash Bars

---
+++ Assets/Scripts/Tools/HUDManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public Image HealthBarFiller;
+    public Image DashBarFiller;
+
+    private PlayerModel _playerModel;
+
+    // Use this for initialization
+    void Start()
+    {
+        _playerModel = LevelManager.Instance.GetPlayerGameObject().GetComponent<PlayerModel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHealthBar();
+        UpdateDashBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        HealthBarFiller.fillAmount = _playerModel.GetHealth() / _playerModel.MaxHealth;
+    }
+
+    void UpdateDashBar()
+    {
+        DashBarFiller.fillAmount = _playerModel.GetAvailableDash() / _playerModel.GetTotalDash();
+    }
+}
